430a4bd448e1af6b55f34e3c66ecbda6cc9e5904
Hephzibah <hephzibahijeje89@gmail.com>
2024-04-25 16:04:15 -0400

    TASK 6

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,13 +1,8 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
-function mergeDeeplyElements(page1, page2) {
-    const map1 = Map(page1);
-    const map2 = Map(page2);
+export default function mergeDeeplyElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
 
-    return List.of(map1.mergeWith((oldVal, newVal) => {
-        if (oldVal && oldVal.mergeDeep) {
-            return oldVal.mergeDeep(newVal);
-        }
-        return newVal;
-    }, map2));
+  return map1.mergeDeep(map2);
 }
